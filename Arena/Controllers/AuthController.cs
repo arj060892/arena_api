ef6e2a1fe400e3f4cc6fb2bfb494312c369aaca3
arj060892 <arj060892@gmail.com>
2018-11-30 12:00:30 +0530

    inital commit with jwt authentication

---
+++ Arena/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Arena.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Arena.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        // GET: /<controller>/
+        [HttpPost("token")]
+        public IActionResult Token([FromBody] Users user)
+        {
+            var username = user.Username;
+            var password = user.Password;
+
+
+            //if (username=="akh" && password=="af")
+            //{
+            var claimData = new[] { new Claim(ClaimTypes.Name, username) };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("asdfawefwewrwwefjnwkf"));
+            var signInCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+            var token = new JwtSecurityToken(
+                issuer: "mysite.com",
+                audience: "mysite.com",
+                expires: DateTime.Now.AddMinutes(3),
+                claims: claimData,
+                signingCredentials: signInCred
+                );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(tokenString);
+
+            //}
+            //return BadRequest("wrong request");
+
+
+            //}
+        }
+    }
+}
